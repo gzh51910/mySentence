61f369ced4271c192644e4d0ddf980e3c199e023
qjunx <52962309+qjunx@users.noreply.github.com>
2019-11-29 21:06:51 +0800

    成功2.0

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import { withToken } from "../utils/hoc";
 import "../Css/home.css";
-import { WingBlank, Carousel } from "antd-mobile";
+import { Carousel } from "antd-mobile";
+import { my } from '../Api';
 @withToken
 class Home extends Component {
     state = {
         data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
         imgHeight: 500,
+    }
+    async componentDidMount() {
+        let data = await my.get("goods/id", {
+            id: '5ddf97497b21000060000f52'
+        })
+        console.log(data);
+
+    }
+    componentDidUpdate() {
+
+        // console.log(111);
+        // var submitObj = document.getElementsByClassName('slider-list');.
+        // console.log(submitObj);
     }
     render() {
         return (
@@ -21,11 +35,22 @@ class Home extends Component {
                         cellSpacing={30}
                         slideWidth={0.8}
                         dots={false}
-                        beforeChange={(from, to) =>
-                            console.log(1)
+                        beforeChange={(from, to) => {
+                            // console.log(from, to);
+
+                            if (to == "9") {
+                                // var submitObj = document.getElementsByClassName('slider-list')[0]
+                                // console.log(submitObj.style.transform);
+                                // document.getElementsByClassName('slider-list')[0].classList.add("ban");
+                                // submitObj.style = "transform: translate3d(-0.2px, 0px, 0px)!important;"
+                            }
+                        }
 
                         }
-                        afterChange={index => this.setState({ slideIndex: index })
+                        afterChange={index => this.setState({ slideIndex: index },
+                            console.log(1)
+
+                        )
                         }
                     >
                         {this.state.data.map((val, index) => (
@@ -35,9 +60,11 @@ class Home extends Component {
                                 style={{
                                     display: 'block',
                                     position: 'relative',
-                                    top: this.state.slideIndex === index ? -10 : 0,
+                                    top: this.state.slideIndex === index ? -15 : 0,
                                     height: this.state.imgHeight,
                                     boxShadow: ' 0px 5px 20px 5px rgba(0, 0, 0, 0.2)',
+                                    borderRadius: "5%",
+                                    overflow: "hidden"
                                 }}
                             >
                                 <img src={`https://zos.alipayobjects.com/rmsportal/IJOtIlfsYdTyaDTRVrLI.png`} />
