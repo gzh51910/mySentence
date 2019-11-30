ebda7b32ea4c1430fdaf957e11411b4826187579
qjunx <52962309+qjunx@users.noreply.github.com>
2019-11-30 14:55:06 +0800

    测试

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,10 @@ import "../Css/home.css";
 import { Carousel } from "antd-mobile";
 import { my } from '../Api';
 @withToken
+
 class Home extends Component {
     state = {
-        data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
+        data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
         imgHeight: 500,
     }
     // async componentDidMount() {
@@ -16,39 +17,33 @@ class Home extends Component {
     //     console.log(data);
 
     // }
-    componentDidUpdate() {
-
-        // console.log(111);
-        // var submitObj = document.getElementsByClassName('slider-list');.
-        // console.log(submitObj);
-    }
     render() {
         return (
             <div className="home">
                 <div className="top subside">
                     <h3>精选投稿</h3>
-                    <i>1</i>
+                    <i className="iconfont icon-diqiu1"></i>
                 </div>
                 <div className="mid">
                     <Carousel className="space-carousel"
                         frameOverflow="visible"
-                        cellSpacing={30}
+                        cellSpacing={20}
                         slideWidth={0.8}
                         dots={false}
                         beforeChange={(from, to) => {
-                            // console.log(from, to);
+                            // console.log(1);
 
                             if (to == "9") {
-                                // var box = document.getElementsByClassName("box");  box[0].value; 
-                                var submitObj = document.getElementsByClassName('slider-list')[0]
-                                submitObj.setAttribute('.bb', 'background: red')
-                                // // document.getElementsByClassName('slider-list')[0].classList.add("ban");
+                                // var submitObj = document.getElementsByClassName('slider-list')[0]
+                                // // // document.getElementsByClassName('slider-list')[0].classList.add("ban");
                                 // submitObj.style.cssText += "transform: translate3d(0px, 0px, 0px)!important;"
                             }
                         }
 
                         }
                         afterChange={index => this.setState({ slideIndex: index },
+                            console.log(1)
+
                         )
                         }
                     >
