021fc3230f4defcd7f88d8f2bbbc887e714462d3
nichousha9 <52962475+nichousha9@users.noreply.github.com>
2019-12-02 20:44:28 +0800

    12313

--- src/pages/Mine.js
+++ src/pages/Mine.js
@@ -1,22 +1,96 @@
-import React, { Component } from 'react';
-import { withToken } from '../utils/hoc';
+import React, { Component } from "react";
+import { withToken } from "../utils/hoc";
+import { Button, Icon } from "antd-mobile";
+import "../Css/find.css";
 @withToken
 class Mine extends Component {
-    constructor() {
-        super()
-        this.state = {
-            hanhong: ''
-        }
-    }
+    state = {
+        message: [
+            {
+                name: "我的信息",
+                icon: "icon-xinxi"
+            },
+            {
+                name: "帮助中心",
+                icon: "icon-bangzhuzhongxin"
+            },
+            {
+                name: "参与句子控",
+                icon: "icon-canyu"
+            },
+            {
+                name: "意见反馈",
+                icon: "icon-fankui"
+            },
+            {
+                name: "设置",
+                icon: "icon-shezhi"
+            },
+            {
+                name: "第三方绑定",
+                icon: "icon-sangeren"
+            },
+            {
+                name: "关于",
+                icon: "icon-guanyu"
+            }
+        ]
+    };
     render() {
-        console.log(111);
-        
-        // console.log("13", this.props);
-        // console.log(this.bb);
-
         return (
-            <div>我的({this.state.hanhong})</div>
-        )
+            <div style={{ height: "100%" }}>
+                <h4
+                    style={{
+                        height: "6%",
+                        textAlign: "center",
+                        background: "#fb7299",
+                        color: "#fff",
+                        fontWeight: "bold",
+                        fontSize: "14px",
+                        lineHeight: "250%"
+                    }}
+                >
+                    账号
+                </h4>
+                <div className="mine-login">
+                    <img
+                        src="../img/juzi.webp"
+                        style={{
+                            height: "3rem",
+                            width: "3rem"
+                        }}
+                    ></img>
+                    <Button
+                        type="primary"
+                        inline
+                        style={{
+                            background: "#fb7299",
+                            fontSize: "12px"
+                        }}
+                        onClick={() => this.props.history.push('/login')}
+                    >
+                        点击登录
+                    </Button>
+                </div>
+                <ul className="mine-nei">
+                    {this.state.message.map(item => {
+                        return <li className={"mine-nei-li"}>
+                        <i className={`iconfont ${item.icon}`}></i>
+                            <p>{item.name}</p>
+                        <Icon
+                            type="right"
+                            style={{
+                                position: "absolute",
+                                top: "22%",
+                                right: "2%"
+                            }}
+                        />
+                    </li>
+                    })}
+                    
+                </ul>
+            </div>
+        );
     }
 }
 // Mine = withToken(Mine);
